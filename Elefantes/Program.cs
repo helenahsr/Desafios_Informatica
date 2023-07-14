6d6aaa3ccaa456c403be5ea81e7d6fb616a180dc
Helena Rezende <99421906+GalaxyHG@users.noreply.github.com>
2023-07-14 09:38:09 -0300

    Add files via upload

---
+++ Elefantes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elefantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numElefantes, elefantes, setIncomoda;
+
+            Console.WriteLine("Digite o número de elefantes: ");
+            numElefantes = int.Parse(Console.ReadLine());
+
+            for (elefantes = 1; elefantes <= numElefantes; elefantes++)
+            {
+                if (elefantes % 2 != 0)
+                {
+                    if (elefantes == 1)
+                    {
+                        Console.WriteLine($"\n{elefantes} elefante incomoda muita gente!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{elefantes} elefantes incomodam muita gente!");
+                    }
+                }
+                else if (elefantes % 2 == 0)
+                {
+                    Console.Write($"{elefantes} elefantes ");
+                    setIncomoda = 1;
+                    while (setIncomoda <= elefantes)
+                    {
+                        Console.Write("incomodam, ");
+                        setIncomoda++;
+                    }
+                    Console.WriteLine("muito mais!");
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
